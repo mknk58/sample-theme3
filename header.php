816a953d0c035ae69da4f82dbe31b56fa572bfec
mknk58 <mk-5858-bf@ezweb.ne.jp>
2018-08-16 21:08:34 +0900

    20180816 新原さんその１

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+<title>展覧会について｜日本の景色写真展</title>
+<link rel="stylesheet" href="css/common.css">
+<link rel="stylesheet" href="css/style.css">
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?> >
+  <header id="top">
+    <h1>
+      <a href="<?php echo home_url( '/' ); ?>">
+        <?php
+				 
+				 ?>
+      </a>
+    </h1>
+  </header>
+	<nav>
+		<ul>
+			<li class="nav_introduction"><a href="introduction.html">展覧会について</a></li>
+			<li class="nav_schedule"><a href="schedule.html">スケジュール</a></li>
+			<li class="nav_gallery"><a href="gallery.html">作品紹介</a></li>
+			<li class="nav_tickets"><a href="tickets.html">前売り券申し込み</a></li>
+		</ul>
+	</nav>
